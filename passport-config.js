497a3d9c82fc180d3422b8ef6e64c95e079d5783
Magn8011 <91495188+Magn8011@users.noreply.github.com>
2022-05-05 21:26:58 +0200

    commits

--- passport-config.js
+++ passport-config.js
@@ -12,13 +12,13 @@ function initialize(passport, getUserByEmail, getUserById) {
     const authenticateUser = async (email, password, done) => {
         const user = getUserByEmail(email)
         if(user == null){
-            return done(null, false, {besked: 'Der findes ikke en bruger som har denne e-mail!'})
+            return done(null, false, {message: 'Der findes ikke en bruger som har denne e-mail!'})
         }
         try{
             if(await bcrypt.compare(password, user.password)){
                 return done(null, user)
             } else{
-                return done(null, false,{besked: 'Der findes ikke en bruger med dette kodeord!'})
+                return done(null, false,{message: 'Der findes ikke en bruger med dette kodeord!'})
             }
         } catch (e){
             return done(e)
