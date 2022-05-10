8dea07519d6e945929a76835cce881eb51ba6d12
Magn8011 <91495188+Magn8011@users.noreply.github.com>
2022-05-10 11:26:57 +0200

    rettelser

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
   }
-  //hent forskellige biblioteker, og angiv dem som variabler 
+  //Hent forskellige biblioteker, og angiv dem som variabler 
   var express = require('express');
   var fetch = require('node-fetch');
   var router = express.Router();
   
-  //angiver hvilke data, som skal fetches via APIkald, dette gøres på userens "id"
+  //Angiver hvilke data, som skal fetches via APIkald, dette gøres på userens "id"
   //Derudover hvordan denne data formateres til json-format og brugeren bliver "renderet" til "index.ejs" siden
   router.get("/", async (req, res) => {
     if (req.isAuthenticated()) {
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV !== "production") {
     var response = await fetch(process.env.API + '/categories');
     const CategoryData = await response.json();
     res.render("index.ejs", {
-  //angiver produktdata og categorylist
+  //Angiver produktdata og categorylist
       Ads: AdsData.data,
       categories: CategoryData.data,
       auth: req.isAuthenticated(),
