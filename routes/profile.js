945f926b92313cafe595fdd6562f4f3ec4574bd1
Magn8011 <91495188+Magn8011@users.noreply.github.com>
2022-05-10 12:36:16 +0200

    ændringer i routes

--- routes/profile.js
+++ routes/profile.js
@@ -5,12 +5,14 @@ if (process.env.NODE_ENV !== "production") {
   var fetch = require("node-fetch");
   var router = express.Router();
   
-  /*
+/*
     Opdatere profil
     brugerprofilerne kommer fra passport-biblioteket og findes i req.user objektet.
     "profile.js" tilhører routes og er derfor en del af klienten.
-  */
-  
+    sidst kunne vi bare kalde dataen fra de flade jsonfiler, nu skal den fetche dataen via apikald
+    hertil hvordan man bliver renderet til enten profile eller login siderne.
+*/
+
   router.get("/", async (req, res) => {
     if (req.isAuthenticated()) {
       const response = await fetch(process.env.API + "/user/" + req.user.Id);
@@ -24,6 +26,7 @@ if (process.env.NODE_ENV !== "production") {
     }
   });
   
+  //her angives der at den data der vises når man er logget ind, er ens egen data, altså ens egne ads/ announcements
   router.post("/", async (req, res) => {
     if (req.isAuthenticated()) {
       const requestOptions = {
